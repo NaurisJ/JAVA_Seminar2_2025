021aa99923a0fc7598fb06a0b86849e79470123f
NaurisJ <nauris.javalds@gmail.com>
2025-02-19 10:33:39 +0200

    Added setters/getters

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,5 +1,40 @@
 package model;
 
 public class Student {
-
+	
+	// 1. mainigie
+	private long stID;
+	private String name;
+	private String surname;
+	
+	
+	// 2. getters
+	
+	public long getstID() {
+		return stID;
+	}
+	public String getName() {
+		return name;
+	}
+	public String setSurname() {
+		return surname;
+	}
+	// 3. setters
+	public void setName(String inputName) {
+		if (inputName != null
+				&& inputName.matches("[A-Z]{1}[a-z ]{1,10}[A-Z]{1}[a-z]{1,10}")) {
+			name = inputName;
+		}
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	
+	
+	// 4. bez argumenta konstruktors
+	// 5. argumenta konstruktors
+	// 6. toString funkcija
+	// 7. citas funkciajs (ja nepieciešams)
+	
+	
 }
