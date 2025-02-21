0fa82c6583ead87771b6d6829bceb99c202c1e94
NaurisJ <nauris.javalds@gmail.com>
2025-02-21 10:17:00 +0200

    Edit: added ArrayList<> and other

--- src/service/MainService.java
+++ src/service/MainService.java
@@ -5,9 +5,17 @@ import model.Grade;
 import model.Professor;
 import model.Student;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import model.Course;
 
 public class MainService {
+	
+	private static ArrayList<Student> allStudents = new ArrayList<Student>();
+	private static ArrayList<Professor> allProfessors = new ArrayList<Professor>();
+	private static ArrayList<Course> allCourses = new ArrayList<Course>();
+	private static ArrayList<Grade> allGrades = new ArrayList<Grade>();
 
 	public static void main(String[] args) {
 		System.out.println("-----------------------------------STUDENTI-------------------------------");
@@ -23,6 +31,9 @@ public class MainService {
 		Student st4 = new Student("23895823", "2347534789");
 		System.out.println(st4);
 		
+		allStudents.addAll(Arrays.asList(st1, st2, st3, st4));
+		System.out.println(allStudents);
+		
 		
 		System.out.println("-----------------------------------PROFESORI--------------------------------");
 		Professor p1 = new Professor();
@@ -30,6 +41,8 @@ public class MainService {
 		
 		Professor p2 = new Professor("Andritis", "Smukais", Degree.bsc);
 		System.out.println(p2);
+		
+		allProfessors.addAll(Arrays.asList(p1, p2));
 		
 		
 		System.out.println("-----------------------------------KURSI---------------------------------------");
@@ -39,6 +52,8 @@ public class MainService {
 		Course c2 = new Course("JAVA",6,p2);
 		System.out.println(c2);
 		
+		allCourses.addAll(Arrays.asList(c1,c2));
+		
 		
 		System.out.println("-----------------------------------ATZIMES----------------------------------------");
 		Grade g1 = new Grade();
@@ -46,6 +61,8 @@ public class MainService {
 		
 		Grade g2 = new Grade(c2,st3,7);
 		System.out.println(g2);
+		
+		allGrades.addAll(Arrays.asList(g1,g2));
 		
 
 	}
